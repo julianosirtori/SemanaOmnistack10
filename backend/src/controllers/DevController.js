1ddf57ede67610b2d049b16bef4ae35506846394
Juliano Sirtori <julianosirtori@gmail.com>
2020-01-16 10:41:45 -0300

    Criando a Base da Aplicação

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,45 @@
+const api = require('../services/api');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+class DevController{
+  async index(request, response){
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  }
+
+  async store(request, response) {
+    const {github_username, techs, latitude, longitude} = request.body;
+
+    let dev = await Dev.findOne({github_username});
+
+    if(!dev){
+      const apiResponse = await api.get(`/users/${github_username}`);
+
+      const {name = login, bio, avatar_url} = apiResponse.data;
+
+      const techsArray = parseStringAsArray(techs);
+
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      }
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
+
+
+
+    return response.json(dev);
+  }
+}
+
+module.exports = new DevController();
